bc42bd137f63e21b7f8ed0d9d20ad864d4493e28
mijip0320 <mijip0320@gmail.com>
2022-09-19 22:41:41 +0900

    get, set 테스트 완료

--- src/main/java/com/example/testApi/controller/HelloController.java
+++ src/main/java/com/example/testApi/controller/HelloController.java
@@ -3,10 +3,13 @@ package com.example.testApi.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
+@RestController
+@RequestMapping("/helloworld")
 public class HelloController {
     /*
     1. 화면에 helloworld가 출력됩니다.
@@ -32,6 +35,28 @@ public class HelloController {
     @GetMapping(value = "/helloworld/page")
     public String helloworld() {
         return "helloworld";
+    }
+
+    @GetMapping(value = "/helloworld/commentGet")
+    @ResponseBody
+    public String commentGet() {
+        return "comment returned";
+    }
+
+    @PostMapping(value = "/helloworld/comment")
+    @ResponseBody
+    public String post(@RequestBody Map<String, String> requestData){
+
+        final String[] keyy = {""};
+        final String[] val = {""};
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            keyy[0] = key;
+            System.out.println("value : " + value);
+            val[0] = value;
+        });
+        return keyy[0] + " " + val[0];
+        //return requestData;
     }
 
     @Setter
